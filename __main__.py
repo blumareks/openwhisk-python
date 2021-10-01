5a9f8e216c6d976d171065010067af675ed0f9ba
blumareks <blumareks@users.noreply.github.com>
2021-10-01 07:29:17 -0700

    initial commit

---
+++ __main__.py
@@ -0,0 +1,15 @@
+from yattag import Doc
+doc, tag, text = Doc().tagtext()        
+
+import searchfile
+#import callselenium
+def main(args):
+  with tag("html"):                     
+    with tag("body"):
+      with tag("h1"):
+        text("search: %s" %
+        searchfile.searchfile(args.get("url"))
+#	  callselenium.callselenium(args.get("url")))
+  return {
+    "body": doc.getvalue()             
+  }
---
+++ searchfile.py
@@ -0,0 +1,4 @@
+def searchfile(url):
+    if url:
+        return "this is %s" % url
+    return "no url"
